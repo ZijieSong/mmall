b6cf40946d5697c9b897501e1ce756bcf318a25f
mac-szj <393593720@qq.com>
2019-02-16 12:03:25 +0800

    commit spring session

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,55 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.UserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.ShardedRedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+@RequestMapping("/user/springSession")
+public class UserSpringSessionController {
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
+        ServerResponse<User> serverResponse = userService.login(username, password);
+        if (serverResponse.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, serverResponse.getData());
+        }
+        return serverResponse;
+    }
+
+    @RequestMapping(value = "/logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.success();
+    }
+
+
+    @RequestMapping(value = "/get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute(Const.CURRENT_USER) == null)
+            return ServerResponse.fail("用户未登陆");
+        return ServerResponse.successByData((User)session.getAttribute(Const.CURRENT_USER));
+    }
+
+}
