7ae78f29d119e858e634500e2976c33a64ff5f9c
mac-szj <393593720@qq.com>
2019-02-21 13:43:36 +0800

    sleep

--- src/main/java/com/mmall/task/ProductTask.java
+++ src/main/java/com/mmall/task/ProductTask.java
@@ -58,6 +58,12 @@ public class ProductTask {
         ShardedRedisUtil.expire(Const.RedisLockKey.CLOSE_ORDER_LOCK, millisToSeconds(lockExpireTime));
         log.info("{},开始定时关单",System.currentTimeMillis());
 //        orderService.closeOrder(Integer.valueOf(PropertiesUtil.get("order.expireTime", "2")));
+        //模拟关单操作, 睡10ms
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            log.error("sleep exception",e);
+        }
         log.info("{},结束定时关单",System.currentTimeMillis());
         //释放锁
         ShardedRedisUtil.del(Const.RedisLockKey.CLOSE_ORDER_LOCK);
